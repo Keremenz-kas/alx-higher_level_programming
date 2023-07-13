390bdcf23d8aa5aac4ff8fc0a12b61df9ad531ef
kermenz-kas <keremenzkas@gmail.com>
2023-07-13 05:17:23 -0700

    test driven mandatory tasks done

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module that sums two integers"""
+
+
+def add_integer(a, b=98):
+    """
+    Args:
+    a (int or float): parameter 1
+    b (int or float): parameter 2
+    """
+     if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
+    return (a + b)
